3745cf7c06d8172473ba39a06722a75da9087429
Rafael Nascimento <42211595+ropnascimento@users.noreply.github.com>
2022-11-23 21:10:54 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,12 +1,26 @@
-import unittest
+import pytest
+from wallet import Wallet, InsufficientAmount
 
-@unittest.fixture
-def teste(x):
-  return x + 3
 
-class unitTest(unittest.TestCase):
-  def testEquals(self):
-    self.assertEqual(teste(3),6)
-    
-if __name__ == '__main__':
-  unittest.main()
+def test_default_initial_amount():
+    wallet = Wallet()
+    assert wallet.balance == 0
+
+def test_setting_initial_amount():
+    wallet = Wallet(100)
+    assert wallet.balance == 100
+
+def test_wallet_add_cash():
+    wallet = Wallet(10)
+    wallet.add_cash(90)
+    assert wallet.balance == 100
+
+def test_wallet_spend_cash():
+    wallet = Wallet(20)
+    wallet.spend_cash(10)
+    assert wallet.balance == 10
+
+def test_wallet_spend_cash_raises_exception_on_insufficient_amount():
+    wallet = Wallet()
+    with pytest.raises(InsufficientAmount):
+        wallet.spend_cash(100)
